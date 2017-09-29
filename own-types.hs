0c162432b119494a3280525061e1415196507270
Wink Saville <wink@saville.com>
2017-09-29 09:44:12 -0700

    Add Point

--- own-types.hs
+++ own-types.hs
@@ -3,13 +3,14 @@ import Dbg (db)
 lvl :: Int
 lvl = 4
 
-data Shape = Circle Float Float Float | Rectangle Float Float Float Float deriving (Show)
+data Point = Point Float Float deriving (Show)
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
 surface :: Shape -> Float
-surface (Circle _ _ radius) =
+surface (Circle _ radius) =
         let area = pi * radius ^ 2
         in area `db` lvl $ "radius:=" ++ show radius ++ " area:=" ++ show area
-surface (Rectangle x1 y1 x2 y2) =
+surface (Rectangle (Point x1 y1) (Point x2 y2)) =
         let width = (abs $ x2 - x1)
             height = (abs $ y2 - y1)
             area = width * height
