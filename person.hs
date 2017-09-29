ae615f94f9e3c8b01a3a4ad95e324a6b477b4a37
Wink Saville <wink@saville.com>
2017-09-29 13:13:28 -0700

    Add person.hs which uses records

---
+++ person.hs
@@ -0,0 +1,9 @@
+import Dbg (db)
+
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavor :: String
+                     } deriving (Show)
