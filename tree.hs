0b87a945286543e19cb7b7dfa8d739021d92f775
Wink Saville <wink@saville.com>
2017-09-30 11:21:24 -0700

    Add treeInsert and dbg

--- tree.hs
+++ tree.hs
@@ -1,4 +1,19 @@
+import Dbg (db)
+
+lvl :: Int
+lvl = 4
+
+f :: (Show a) => a -> a
+f x = x `db` lvl $ "f x:=" ++ show x
+
 data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
 
-singleton :: a -> Tree a
-singleton x = Node x EmptyTree EmptyTree
+singleton :: (Show a) => a -> Tree a
+singleton x = Node x EmptyTree EmptyTree `db` lvl $ "singleton x:=" ++ show x
+
+treeInsert :: (Ord a, Show a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x `db` lvl $ "create singleton x:=" ++ show x
+treeInsert x (Node a left right)
+        | x == a = Node x left right `db` lvl $ "replace x:=" ++ show x ++ " == a:=" ++ show a
+        | x < a  = Node a (treeInsert x left) right `db` lvl $ "insert left x:=" ++ show x ++ " < a:=" ++ show a
+        | x > a  = (Node a left (treeInsert x right)) `db` lvl $ "insert right x:=" ++ show x ++ " > a:=" ++ show a
