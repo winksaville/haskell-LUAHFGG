19cc501581f6cac1891e3ad6a3b5c8681ce07ae9
Wink Saville <wink@saville.com>
2017-10-01 16:17:09 -0700

    Remove debugging an do some "performance" testing with ghci.
    Obviously using ghci isn't great but I'm only interested in undestanding
    how haskell executes at the moment.
    
    1) A tree of 1000 item (unbalanced) is HUGE, 375MB if Tree is derived
       from Show, if you don't derive from show it's only 179MB, still LARGE:
    ```
    *Main> nums = [1..1000]
    (0.00 secs, 0 bytes)
    *Main> tree = foldr treeInsert EmptyTree nums
    (0.01 secs, 0 bytes)
    *Main> treeE
    *Main> x = treeElem 1 tree
    (0.01 secs, 0 bytes)
    *Main> x
    True
    (0.24 secs, 179,344,576 bytes)
    ```
    
    2) If you request x a second time it only takes 0.00 secs
    ```
    *Main> x = treeElem 1 tree
    (0.01 secs, 0 bytes)
    *Main> x
    True
    (0.24 secs, 179,346,616 bytes)
    *Main> x
    True
    (0.00 secs, 68,112 bytes)
    ```
    
    3) If you ask for a different element the Tree is rebuilt and takes the
    same amount of time.
    ```
    *Main> y = treeElem 2 tree
    (0.01 secs, 0 bytes)
    *Main> y
    True
    (0.23 secs, 179,345,976 bytes)
    ```

--- tree.hs
+++ tree.hs
@@ -6,17 +6,17 @@ lvl = 4
 f :: (Show a) => a -> a
 f x = x `db` lvl $ "f x:=" ++ show x
 
-data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Read, Eq)
 
-singleton :: (Show a) => a -> Tree a
-singleton x = Node x EmptyTree EmptyTree `db` lvl $ "singleton x:=" ++ show x
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree --`db` lvl $ "singleton x:=" ++ show x
 
-treeInsert :: (Ord a, Show a) => a -> Tree a -> Tree a
-treeInsert x EmptyTree = singleton x `db` lvl $ "create singleton x:=" ++ show x
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x --`db` lvl $ "create singleton x:=" ++ show x
 treeInsert x (Node a left right)
-        | x == a = Node x left right `db` lvl $ "replace x:=" ++ show x ++ " == a:=" ++ show a
-        | x < a  = Node a (treeInsert x left) right `db` lvl $ "insert left x:=" ++ show x ++ " < a:=" ++ show a
-        | x > a  = (Node a left (treeInsert x right)) `db` lvl $ "insert right x:=" ++ show x ++ " > a:=" ++ show a
+        | x == a = Node x left right --`db` lvl $ "replace x:=" ++ show x ++ " == a:=" ++ show a
+        | x < a  = Node a (treeInsert x left) right --`db` lvl $ "insert left x:=" ++ show x ++ " < a:=" ++ show a
+        | x > a  = (Node a left (treeInsert x right)) --`db` lvl $ "insert right x:=" ++ show x ++ " > a:=" ++ show a
 
 treeElem :: (Ord a) => a -> Tree a -> Bool
 treeElem x EmptyTree = False
