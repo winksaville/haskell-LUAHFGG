92883f2accd4cde128c741d8adeeb19beb0978ae
Wink Saville <wink@saville.com>
2017-10-01 15:17:57 -0700

    Add treeElem

--- tree.hs
+++ tree.hs
@@ -17,3 +17,10 @@ treeInsert x (Node a left right)
         | x == a = Node x left right `db` lvl $ "replace x:=" ++ show x ++ " == a:=" ++ show a
         | x < a  = Node a (treeInsert x left) right `db` lvl $ "insert left x:=" ++ show x ++ " < a:=" ++ show a
         | x > a  = (Node a left (treeInsert x right)) `db` lvl $ "insert right x:=" ++ show x ++ " > a:=" ++ show a
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+        | x == a = True
+        | x < a = treeElem x left
+        | x > a = treeElem x right
