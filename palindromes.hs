cb75dcca95e64b9067937fbfd0900368a6082d33
Wink Saville <wink@saville.com>
2017-10-02 13:26:21 -0700

    palindromes

---
+++ palindromes.hs
@@ -0,0 +1,9 @@
+main = interact $ respondPalindromes
+
+respondPalindromes contents =
+    unlines (map (\xs ->
+                    if isPalindrome xs
+                        then "palindrome"
+                        else "not a palindrome")
+                (lines contents))
+    where isPalindrome xs = xs == reverse xs
