6491aa96ef6d13238940d1f56cbfe81101527c6f
Wink Saville <wink@saville.com>
2017-10-02 12:59:54 -0700

    Filter short lines
      $ ghc --make shortlinesonly
      $ cat shortlines | ./shortlinesonly

---
+++ shortlinesonly.hs
@@ -0,0 +1,12 @@
+import Control.Monad
+
+main = do
+    contents <- getContents
+    putStr $ shortLinesOnly contents
+
+shortLinesOnly :: String -> String
+shortLinesOnly input =
+    let allLines = lines input
+        shortLines = filter (\line -> length line < 10) allLines
+        result = unlines shortLines
+    in  result
