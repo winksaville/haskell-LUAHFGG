fa2640f1286b336480388c47e7b6542ec3f83d1d
Wink Saville <wink@saville.com>
2017-10-02 13:33:03 -0700

    Tweak formatting of palindromes

--- palindromes.hs
+++ palindromes.hs
@@ -4,6 +4,5 @@ respondPalindromes =
     unlines . map (\xs ->
                     if isPalindrome xs
                         then "palindrome"
-                        else "not a palindrome")
-                . lines
+                        else "not a palindrome") . lines
     where isPalindrome xs = xs == reverse xs
