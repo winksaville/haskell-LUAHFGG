5f559a9cdd7b602c4403e8a7fb2fd63d7c75a663
Wink Saville <wink@saville.com>
2017-10-02 15:21:54 -0700

    Implement withFile'
    This is how the standard withFile could be implemented.

--- girlfriend.hs
+++ girlfriend.hs
@@ -1,7 +1,13 @@
 import System.IO
 
 main = do
-    handle <- openFile "girlfriend.txt" ReadMode
-    contents <- hGetContents handle
-    putStr contents
+    withFile' "girlfriend.txt" ReadMode (\handle -> do
+        contents <- hGetContents handle
+        putStr contents)
+
+withFile' :: FilePath -> IOMode -> (Handle -> IO a) -> IO a
+withFile' path mode f = do
+    handle <- openFile path mode
+    result <- f handle
     hClose handle
+    return result
